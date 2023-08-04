00545d9a5dd35111c41125c1c56fb9c947a8a610
AbdallahAlUbaidi <Abdallah.Alubaidi111@gmail.com>
2023-08-04 13:51:57 +0300

    Added Echo endpoint that echos a word from the url

--- myApp.js
+++ myApp.js
@@ -27,6 +27,9 @@ app.get("/now", (req, res, next) => {
     res.json({ time: req.time });
 });
 
+app.get("/:word/echo", (req, res) => {
+    res.json({ echo: req.params.word });
+});
 
 
 function logger(req, res, next) {
