048188d03223885d34beb4eb1d9f1e479c781383
AbdallahAlUbaidi <Abdallah.Alubaidi111@gmail.com>
2023-08-04 10:44:40 +0300

    C4:Added static assets functionality

--- myApp.js
+++ myApp.js
@@ -5,6 +5,9 @@ let app = express();
 
 console.log("Hello World");
 
+const staticDir = __dirname + "/public";
+app.use(express.static(staticDir));
+
 app.get("/", (req, res) => {
     let apsolutePath = __dirname + "/views/index.html";
     res.sendFile(apsolutePath);
