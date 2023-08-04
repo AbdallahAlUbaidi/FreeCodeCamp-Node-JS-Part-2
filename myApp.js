3fca97be9aecb712fe606eeaa9cd0ef02b424e78
AbdallahAlUbaidi <Abdallah.Alubaidi111@gmail.com>
2023-08-04 10:51:22 +0300

    C4F2:Fixed The staticDir string

--- myApp.js
+++ myApp.js
@@ -5,8 +5,7 @@ let app = express();
 
 console.log("Hello World");
 
-const staticDir = __dirname + "/public/";
-app.use(express.static(staticDir));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     let apsolutePath = __dirname + "/views/index.html";
